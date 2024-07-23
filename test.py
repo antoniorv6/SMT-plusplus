a9f596ba00a0be1550bb74d01bda07afb362c1e4
antoniorv6 <ariosvila@gmail.com>
2024-07-23 10:11:50 +0200

    feat(test): Included individual test file

---
+++ test.py
@@ -0,0 +1,27 @@
+import torch
+from data import FinetuningDataset
+from smt_trainer import SMTPP_Trainer
+
+from lightning.pytorch import Trainer
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+
+torch.set_float32_matmul_precision('high')
+
+def main():
+    data = FinetuningDataset(vocab_name='Mozarteum_BeKern', tokenization_mode='bekern')
+    model_wrapper = SMTPP_Trainer.load_from_checkpoint('weights/finetuning/SMTPP_Mozarteum_Bekern_f0.ckpt')
+
+    trainer = Trainer(max_epochs=100000, 
+                      check_val_every_n_epoch=3500, precision='16-mixed')
+    
+    data.train_dataset.set_trainer_data(trainer)
+
+    trainer.test(model_wrapper, datamodule=data)
+    
+    model_wrapper.model.save_pretrained("SMTPP", variant="Mozarteum_BeKern_fold0")
+    
+
+if __name__ == "__main__":
+    main()
